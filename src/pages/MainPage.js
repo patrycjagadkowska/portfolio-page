398cd7e0da0ec93b0a00b1894968cf8b96b65b69
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2023-02-09 11:02:35 +0000

    add animation to main page

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,9 +2,16 @@ import classes from './styles/MainPage.module.css';
 
 const MainPage = () => {
     return (
-        <div className={classes['main-page__header']}>
-            <h1>Hi!<br/>I'm Patrycja<br/>and I'm React Frontend Developer</h1>
-        </div>
+        <>
+            <div className={classes['main-page__header']}>
+                <h1>Hi!<br/>I'm Patrycja<br/>and I'm React Frontend Developer</h1>
+            </div>
+            <div className={classes['animations']}>
+                <div className={classes['animations__circle']} />
+                <div className={classes['animations__circle']} />
+                <div className={classes['animations__circle']} />
+            </div>
+        </>
     );
 };
 
