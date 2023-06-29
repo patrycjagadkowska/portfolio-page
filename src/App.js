58c7e0c83a7450eaba3aad543fe0fdab35a5d564
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2023-06-29 13:37:08 +0100

    simplify router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Layout from "./components/Layout/Layout";
-import MainPage from './pages/MainPage';
-import ContactPage from './pages/ContactPage';
-import PortfolioPage from './pages/PortfolioPage';
-import AboutPage from './pages/AboutPage';
+import MainPage from "./pages/MainPage";
+import ContactPage from "./pages/ContactPage";
+import PortfolioPage from "./pages/PortfolioPage";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
-  const router = createBrowserRouter(createRoutesFromElements(
-    <Route path='/' element={<Layout />}>
-        <Route index element={<MainPage />} />,
-        <Route path='about' element={<AboutPage />} />,
-        <Route path='portfolio' element={<PortfolioPage />} />,
-        <Route path='contact' element={<ContactPage />} />
-    </Route>
-  ));
-  return (
-    <RouterProvider router={router}></RouterProvider>
-  );
-};
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { index: true, element: <MainPage /> },
+        { path: "about", element: <AboutPage /> },
+        { path: "portfolio", element: <PortfolioPage /> },
+        { path: "contact", element: <ContactPage /> },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router}></RouterProvider>;
+}
 
 export default App;
