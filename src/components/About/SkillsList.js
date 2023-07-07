76a703cdb21a4c7e29e2eb58fe1c9fde103f1ea8
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2023-07-07 11:54:30 +0100

    simplify lists in about me

--- src/components/About/SkillsList.js
+++ src/components/About/SkillsList.js
@@ -1,39 +1,28 @@
-import {
-  FaHtml5,
-  FaCss3Alt,
-  FaReact,
-  FaBootstrap
-} from "react-icons/fa";
+import { FaHtml5, FaCss3Alt, FaReact, FaBootstrap } from "react-icons/fa";
 import { SiJavascript } from "react-icons/si";
 import { FcSmartphoneTablet } from "react-icons/fc";
-import { useEffect, useState, useMemo } from "react";
 
 import ListItem from "./ListItem";
 
 import classes from "./styles/AboutLists.module.css";
 
 const SkillsList = () => {
-  const [itemsList, setItemsList] = useState([]);
+  const skillsList = [
+    { icon: <FaHtml5 />, title: "HTML5" },
+    { icon: <FaCss3Alt />, title: "CSS3" },
+    { icon: <SiJavascript />, title: "JavaScript" },
+    { icon: <FaReact />, title: "React" },
+    { icon: <FaBootstrap />, title: "Bootstrap" },
+    { icon: <FcSmartphoneTablet />, title: "RWD" },
+  ];
 
-  const skillsList = useMemo(() => {
-    return [
-        { icon: <FaHtml5 />, title: "HTML5" },
-        { icon: <FaCss3Alt />, title: "CSS3" },
-        { icon: <SiJavascript />, title: "JavaScript" },
-        { icon: <FaReact />, title: "React" },
-        { icon: <FaBootstrap />, title: "Bootstrap" },
-        { icon: <FcSmartphoneTablet />, title: "RWD" },
-      ]
-  }, []);
+  const mappedItems = skillsList.map((item, index) => {
+    return (
+      <ListItem icon={item.icon} title={item.title} key={`skill${index}`} />
+    );
+  });
 
-  useEffect(() => {
-    const mappedItems = skillsList.map((item, index) => {
-      return <ListItem icon={item.icon} title={item.title} key={`skill${index}`} />;
-    });
-    setItemsList(mappedItems);
-  }, [skillsList, setItemsList]);
-
-  return <ul className={classes["icons-list"]}>{itemsList}</ul>;
+  return <ul className={classes["icons-list"]}>{mappedItems}</ul>;
 };
 
 export default SkillsList;
--- src/components/About/ToolsList.js
+++ src/components/About/ToolsList.js
@@ -1,32 +1,24 @@
 import { FaGitAlt, FaGithub } from "react-icons/fa";
 import { SiVisualstudiocode } from "react-icons/si";
-import { useState, useEffect, useMemo } from "react";
 
 import ListItem from "./ListItem";
 
 import classes from "./styles/AboutLists.module.css";
 
 const ToolsList = () => {
-  const [itemsList, setItemsList] = useState([]);
+  const toolsList = [
+    { icon: <FaGitAlt />, title: "Git" },
+    { icon: <FaGithub />, title: "Github" },
+    { icon: <SiVisualstudiocode />, title: "Visual Studio Code" },
+  ];
 
-  const toolsList = useMemo(() => {
-    return [
-      { icon: <FaGitAlt />, title: "Git" },
-      { icon: <FaGithub />, title: "Github" },
-      { icon: <SiVisualstudiocode />, title: "Visual Studio Code" },
-    ];
-  }, []);
+  const mappedArray = toolsList.map((item, index) => {
+    return (
+      <ListItem icon={item.icon} title={item.title} key={`tool${index}`} />
+    );
+  });
 
-  useEffect(() => {
-    const mappedArray = toolsList.map((item, index) => {
-      return (
-        <ListItem icon={item.icon} title={item.title} key={`tool${index}`} />
-      );
-    });
-    setItemsList(mappedArray);
-  }, [toolsList, setItemsList]);
-
-  return <ul className={classes["icons-list"]}>{itemsList}</ul>;
+  return <ul className={classes["icons-list"]}>{mappedArray}</ul>;
 };
 
 export default ToolsList;
