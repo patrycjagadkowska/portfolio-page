c5dd44dfeaed8cba1ebebafaea10e12c9aaed181
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2023-07-07 11:31:25 +0100

    add lazy loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
 import Layout from "./components/Layout/Layout";
 import MainPage from "./pages/MainPage";
-import ContactPage from "./pages/ContactPage";
-import PortfolioPage from "./pages/PortfolioPage";
-import AboutPage from "./pages/AboutPage";
+// import ContactPage from "./pages/ContactPage";
+// import PortfolioPage from "./pages/PortfolioPage";
+// import AboutPage from "./pages/AboutPage";
+
+const ContactPage = lazy(() => import("./pages/ContactPage"));
+const PortfolioPage = lazy(() => import("./pages/PortfolioPage"));
+const AboutPage = lazy(() => import("./pages/AboutPage"));
 
 function App() {
   const router = createBrowserRouter([
@@ -13,9 +18,9 @@ function App() {
       element: <Layout />,
       children: [
         { index: true, element: <MainPage /> },
-        { path: "about", element: <AboutPage /> },
-        { path: "portfolio", element: <PortfolioPage /> },
-        { path: "contact", element: <ContactPage /> },
+        { path: "about", element: <Suspense><AboutPage /></Suspense> },
+        { path: "portfolio", element: <Suspense><PortfolioPage /></Suspense> },
+        { path: "contact", element: <Suspense><ContactPage /> </Suspense>},
       ],
     },
   ]);
